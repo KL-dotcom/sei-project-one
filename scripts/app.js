142ba95d7624d832c5d0023ccd335674e0670def
Krissy Little <kristenlittle1992@gmail.com>
2020-04-10 22:10:35 +0100

    added methods for frog and truck, working on the collision

--- scripts/app.js
+++ scripts/app.js
@@ -6,8 +6,8 @@ function init() {
   const height = 10
   const width = 10
 
-  let frogPositionX = 0
-  let frogPositionY = 0
+  // let frogPositionX = 0
+  // let frogPositionY = 0
 
   function createCells() {
     for (let y = 0; y < height; y++) {
@@ -19,112 +19,85 @@ function init() {
         cells[y][x] = cell
       }
     }
-    cells[frogPositionY][frogPositionX].classList.add('frog')
+
   }
   createCells(0)
 
   //* some kind of thing that can move around the grid
   // execution
 
-
-  function handleKeyUp(event) {
-    cells[frogPositionY][frogPositionX].classList.remove('frog')
-    switch (event.keyCode) {
-      case 39:
-        if (frogPositionX < width - 1) frogPositionX++
-        console.log('move right')
-        break
-      case 37:
-        if (frogPositionX > 0) frogPositionX--
-        console.log('move left')
-        break
-      case 38:
-        if (frogPositionY > 0) frogPositionY--
-        console.log('move up')
-        break
-      case 40:
-        if (frogPositionY < width - 1) frogPositionY++
-        console.log('move down')
-        break
-      default:
-        console.log('nope')
+  const frog = {
+    lives: 3,
+    positionY: 0,
+    positionX: 0,
+    keyPressed: null,
+    handlePlayerInput(event) {
+      frog.keyPressed = event.keyCode
+    },
+    move() {
+      cells[frog.positionY][frog.positionX].classList.remove('frog')
+      const lastKeyPressed = this.keyPressed
+      this.keyPressed = null
+      switch (lastKeyPressed) {
+        case 39:
+          if (this.positionX < width - 1) this.positionX++
+          console.log('move right')
+          break
+        case 37:
+          if (this.positionX > 0) this.positionX--
+          console.log('move left')
+          break
+        case 38:
+          if (this.positionY > 0) this.positionY--
+          console.log('move up')
+          break
+        case 40:
+          if (this.positionY < width - 1) this.positionY++
+          console.log('move down')
+          break
+        default:
+          console.log('no player input')
+      }
+      cells[frog.positionY][frog.positionX].classList.add('frog')
     }
-    cells[frogPositionY][frogPositionX].classList.add('frog')
+
   }
 
-
-  // event listener
-
-
-  document.addEventListener('keyup', handleKeyUp)
-
-  //* some kind of thing that checks for a condition where two classes are in the same div and plays some kind of collision thing
-
-
-  // execution
-  function collision(event) {
-    if (!event.target.classList.contains('truck')) return
-    console.log('boom')
-  }
-
-  // event
-
-  document.addEventListener('keyup', collision)
-
-  //* some kind of thing that goes accross the screen on a loop I guess
-
-  //define a loop
-  let truckPositionX = 0
-  const truckPositionY = 8
-
-  cells[truckPositionY][truckPositionX].classList.add('truck')
-
-  let num = 0
-
-  const truckTimer = setInterval(() => {
-    num++
-    if (num > 9) {
-      clearInterval(truckTimer)
-    } else {
-
-      console.log('I am a truck out to get you ' + num)
-      cells[truckPositionY][truckPositionX].classList.remove('truck')
-      truckPositionX++
-      cells[truckPositionY][truckPositionX].classList.add('truck')
-
-      //* some kind of thing where it says the player has won once the player moves into a certain area on the game board
-
-
-      //* some kind of start button that activates the above, maybe something that could also come up as a 'play again' type of thing
-
-
-      //* maybe a timer or something that would finish it all, as well as being finished by winning
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  const truck = {
+    positionY: 8,
+    positionX: -1,
+    lastMovedAt: Date.now(),
+    move() {
+      if (Date.now() - this.lastMovedAt > 500) {
+        if (this.positionX >= 0) {
+          cells[this.positionY][this.positionX].classList.remove('truck')
+        }
+        this.positionX = (this.positionX + 1) % width
+        cells[this.positionY][this.positionX].classList.add('truck')
+        this.lastMovedAt = Date.now()
+      }
+    },
+    collision() {
+      if (this.positionY === frog.positionY && this.positionX === frog.positionX) {
+        cells[truck.positionY][truck.positionX].classList.add('boom')
+        frog.positionY = 0
+        frog.positionX = 0
+      }
 
 
 
     }
+  }
+
+  const num = 0
+
+  const gameTimer = setInterval(() => {
+    frog.move()
+    truck.move()
+    truck.collision()
+  }, 10)
 
 
-  }, 500)
 
 
 
@@ -132,9 +105,10 @@ function init() {
 
 
 
+  // event listeners
 
 
-
+  document.addEventListener('keyup', frog.handlePlayerInput)
 
 }
 
